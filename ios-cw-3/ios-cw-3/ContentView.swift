1e469246e95a3cf99dffcce0e9902a9276bf3d9f
Talal-Alkhamis-C77 <70804941+Talal-Alkhamis-C77@users.noreply.github.com>
2022-03-05 21:06:45 +0300

    DONE

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -9,8 +9,90 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("Wall")
+            
+        VStack{
+            
+            Text("Here are my favourite characters 😍")
+                .font(.title2)
+                .foregroundColor(.white)
+                
+                
+
+            
+           
+            HStack(alignment: .bottom){
+            Image("1")
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            
+            
+            
+        Image("2")
+                .resizable()
+                .frame(width: 100, height: 100)
+                
+                
+   
+            }
+            
+            HStack(spacing: 95.0){
+                Text("Minions")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                
+                
+                Text("Batman")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                    
+                    
+                    
+                
+            }
+            
+            HStack{
+            Image("3.png")
+                .resizable()
+                .frame(width: 100, height: 100)
+                
+            
+            
+        Image("4")
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            
+            Image("5")
+                .resizable()
+                .frame(width: 100, height: 100)
+            }
+        
+            HStack(spacing: 80.0){
+                Text("Mr.Bean")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                
+                
+                Text("Spider Man")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                
+                Text("Jocker")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                    
+                    
+                    
+                
+            }
+
+        
+        
+        }
+
     }
 }
 
@@ -18,4 +100,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
 }
